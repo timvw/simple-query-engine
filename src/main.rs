ddc12a250dc3695162bfe59534046b5970ac3978
Tim Van Wassenhove <tim@timvw.be>
2022-05-07 16:34:21 +0200

    start fleshing out the required types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use arrow2::datatypes::Schema;
+
 fn main() {
     println!("Hello, world!");
+}
+
+trait DataSource {
+    fn schema(&self) -> Schema;
+}
+
+trait LogicalPlan {
+    fn schema(&self) -> Schema;
+}
+
+trait PhyiscalPlan {
+}
+
+trait QueryPlanner {
+    fn create_physical_plan(logical_plan: dyn LogicalPlan) -> dyn PhyiscalPlan;
 }
