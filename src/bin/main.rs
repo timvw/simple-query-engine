6b3a4404e0a29a327833e0b96b40332f609b42c0
Tim Van Wassenhove <tim@timvw.be>
2022-05-08 00:09:47 +0200

    update main fn to actually print results

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,8 +3,10 @@ use simply_query_engine::datasource::parquet::*;
 use simply_query_engine::logical::*;
 use simply_query_engine::optimiser::QueryOptimiser;
 use simply_query_engine::planner::QueryPlanner;
+use futures::StreamExt;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
 
     let test_file = "/Users/timvw/src/github/simply-query-engine/test-data/alltypes_plain.parquet";
     let datasource = ParquetDataSource::new(test_file.to_string())?;
@@ -14,9 +16,9 @@ fn main() -> Result<()> {
     let optimized_plan = QueryOptimiser::optimize(&scan);
 
     let phyiscal_plan = QueryPlanner::create_physical_plan(optimized_plan);
-    let result = phyiscal_plan.execute();
 
-    for item in result {
+    let mut result = phyiscal_plan.execute();
+    for item in result.next().await {
         println!("here is a batch: {:?}", item);
     }
     Ok(())
